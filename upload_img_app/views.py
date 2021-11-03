dba4397057dae61e7d68ba7b8bb7d18110e041dd
yjnmk <yj.kur@yf6.so-net.ne.jp>
2021-11-03 15:55:12 +0900

    add function face alignment

--- upload_img_app/views.py
+++ upload_img_app/views.py
@@ -2,35 +2,32 @@ from django.shortcuts import render, redirect
 from .forms import ImageForm
 import dlib
 from static.utils.alignment import align_face
-import os
+import cv2
 
-
-def run_alignment(image_path):
-        # import dlib
-        # from static.utils.alignment import align_face
+# 顔の切り出しの関数
+def run_alignment(image_path, output_path):
         predictor = dlib.shape_predictor('static/shape_predictor_68_face_landmarks.dat')
         aligned_image = align_face(filepath=image_path, predictor=predictor) 
-        return aligned_image
-
+        return aligned_image.save(output_path)
 
 def image_upload(request):
   if request.method == 'POST':
     form = ImageForm(request.POST, request.FILES)
     if form.is_valid():
       form.save()
+    
       image_name  = request.FILES['image']
       image_name2  = request.FILES['image2']
-      image_url = 'media/documents/{}'.format(image_name)
+      image_url = 'media/documents/{}'.format(image_name) 
       image_url2 = 'media/documents2/{}'.format(image_name2)
-      
-      align_image = run_alignment(image_url)
-      align_image2 = run_alignment(image_url2)
-      # align_image.resize((256,256))
-      # align_image2.resize((256,256)
-      align_image.save()
 
-      align_url = 'media/align/{}'.format(align_image)
-      align_url2 = 'media/align2/{}'.format(align_image2)
+      #切り出した画像の保存場所
+      align_url = 'media/align/align.jpg'
+      align_url2 = 'media/align2/agign2.jpg'
+
+      # 切り出しの実行
+      align_image = run_alignment(image_url, align_url)
+      align_image2 = run_alignment(image_url2, align_url2)
 
       return render(request, 'upload_img_app/image.html', {'align_url':align_url, 'align_url2':align_url2})
             
@@ -38,3 +35,5 @@ def image_upload(request):
   else:
     form = ImageForm()
     return render(request, 'upload_img_app/index.html', {'form':form})
+
+
